553c6de9ffa1f3678bcca33bc36630d91f35c7ee
Dean Jones <dmitryi.irk947@gmail.com>
2022-03-10 19:30:29 +0800

    One rub

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialData = 100;
+        int replenishmentAmount = 1100;
+        int total;
+        if (replenishmentAmount >= 1000)
+            total = (int) ((replenishmentAmount * 0.01) + replenishmentAmount + initialData);
+        else { total = (replenishmentAmount + initialData);
+        }
+        System.out.println(total);
+    }
+}
