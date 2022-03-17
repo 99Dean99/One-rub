14f067c87115772cdce01b5d57cf645d2b8bc421
Дмиртий <88325082+99Dean99@users.noreply.github.com>
2022-03-17 20:33:12 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ public class Main {
         int initialData = 100;
         int replenishmentAmount = 1100;
         int total;
-        if (replenishmentAmount >= 1000)
+        if {
+            (replenishmentAmount >= 1000)
             total = (int) ((replenishmentAmount * 0.01) + replenishmentAmount + initialData);
-        else { total = (replenishmentAmount + initialData);
+        }
+        else { 
+            total = (replenishmentAmount + initialData);
         }
         System.out.println(total);
     }
